11b5f06bb72ed875980dd75d1f4c546caac17ec3
Chris <christopherryan91@gmail.com>
2023-08-21 00:17:37 +0100

    Add variable for each team connected to the corresponding cell and update function to update points cell rather than the whole row

---
+++ code.py
@@ -0,0 +1,93 @@
+table = SHEET.worksheet('table')
+fixtures = SHEET.worksheet('fixtures')
+
+
+
+Arsenal = table.find("Arsenal")
+AstonVilla = table.find("Aston Villa")
+Bournemouth = table.find("Bournemouth")
+Brentford = table.find("Brentford")
+Brighton = table.find("Brighton")
+Burnley = table.find("Burnley")
+Chelsea = table.find("Chelsea")
+CrystalPalace = table.find("Crystal Palace")
+Everton = table.find("Everton")
+Fulham = table.find("Fulham")
+Liverpool = table.find("Liverpool")
+Luton = table.find("Luton")
+ManCity = table.find("Manchester City")
+ManUnited = table.find("Manchester United")
+Newcastle = table.find("Newcastle")
+Nottingham = table.find("Newcastle")
+Sheffield = table.find("Sheffield")
+Tottenham = table.find("Tottenham")
+WestHam = table.find("West Ham")
+Wolves = table.find("Wolves")
+
+team_a = (Arsenal, Bournemouth, Chelsea, Brentford, ManUnited)
+
+team_b = (Wolves, WestHam, Brentford, Sheffield)
+
+
+""""
+team_one = fixtures.get('C2:C381')
+team_two = fixtures.get('D2:D381')
+"""
+
+def get_match_winner():
+
+    for (a, b) in zip(team_a, team_b):
+        team_one_score = input(f"Enter {a} score here: \n")
+        team_two_score = input(f"Enter {b} score here: \n")
+
+        if team_one_score > team_two_score:
+
+            first_cell_values = [[str(a), +1 , +1, +0, +0, int(team_one_score), int(team_two_score), +(int(team_one_score)-int(team_two_score)), +3 ]]
+            
+            cell_list = table.range('A1:H1')
+
+            for i, val in enumerate(first_cell_values):  #gives us a tuple of an index and value
+                cell_list[i].value = val    #use the index on cell_list and the val from cell_values
+
+            table.update_cells(cell_list)
+            #a_notation = "A" + str(Luton.row) + ":AT"+ str(Luton.row)
+                
+            #table.update(a_notation, first_cell_values)
+
+        elif team_one_score == team_two_score:
+            first_cell_list = table.range('A2:I2')
+            first_cell_values = [str(a), +1 , +0, +0, +1, int(team_one_score), int(team_two_score), +(int(team_one_score)-int(team_two_score)), +1]
+
+            for i, val in enumerate(first_cell_values):  #gives us a tuple of an index and value
+                first_cell_list[i].value = val    #use the index on cell_list and the val from cell_values
+
+            table.update_cells(first_cell_list)
+
+            second_cell_list = table.range('A3:I3')
+            second_cell_values = [str(b), +1, +0, +0, +1, int(team_two_score), int(team_one_score), +(int(team_two_score)-int(team_one_score)), +1]
+
+            for i, val in enumerate(second_cell_values):  #gives us a tuple of an index and value
+                second_cell_list[i].value = val    #use the index on cell_list and the val from cell_values
+
+            table.update_cells(second_cell_list)
+        else:
+            first_cell_list = table.range('A2:I2')
+            first_cell_values = [str(a), +1 , +0, +1, +0, int(team_one_score), int(team_two_score), +(int(team_one_score)-int(team_two_score)), +0 ]
+
+            for i, val in enumerate(first_cell_values):  #gives us a tuple of an index and value
+                first_cell_list[i].value = val    #use the index on cell_list and the val from cell_values
+
+            table.update_cells(first_cell_list)
+
+            second_cell_list = table.range('A3:I3')
+            second_cell_values = [str(b), +1 , +1, +0, +0, int(team_two_score), int(team_one_score), +(int(team_two_score)-int(team_one_score)), +3 ]
+
+            for i, val in enumerate(second_cell_values):  #gives us a tuple of an index and value
+                second_cell_list[i].value = val    #use the index on cell_list and the val from cell_values
+
+            table.update_cells(second_cell_list)
+
+get_match_winner()
+
+
+"""
--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import gspread 
 from google.oauth2.service_account import Credentials
 
+import numpy as np
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -18,234 +20,120 @@ table = SHEET.worksheet('table')
 fixtures = SHEET.worksheet('fixtures')
 
 
-arsenal = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
+Arsenal = table.acell('A2').value
+AstonVilla = table.acell('A3').value
+Bournemouth = table.acell('A4').value
+Brentford = table.acell('A5').value
+Brighton = table.acell('A6').value
+Burnley = table.acell('A7').value
+Chelsea = table.acell('A8').value
+CrystalPalace = table.acell('A9').value
+Everton = table.acell('A10').value
+Fulham = table.acell('A11').value
+Liverpool = table.acell('A12').value
+Luton = table.acell('A13').value
+ManCity = table.acell('A14').value
+ManUnited = table.acell('A15').value
+Newcastle = table.acell('A16').value
+Nottingham = table.acell('A17').value
+Sheffield = table.acell('A18').value
+Tottenham = table.acell("A19").value
+WestHam = table.acell('A20').value
+Wolves = table.acell('A21').value
 
-aston_villa = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
 
-bournemouth = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
+team_a = (WestHam, Arsenal, ManCity, ManUnited, Chelsea)
 
-brentford = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-brighton = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-burnley = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-chelsea = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-crystal_palace = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-everton = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-fulham = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-liverpool = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-luton_town = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-manchester_city = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-manchester_united = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-newcastle = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-nottingham_forest = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-sheffield_united = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-tottenham = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-west_ham = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
-
-wolves = {
-  "Matches Played": 0,
-  "Win": 0,
-  "Draw": 0,
-  "Loss": 0,
-  "Goals Scored": 0,
-  "Goals Against": 0,
-  "Goal Difference": 0,
-  "Points": 0
-}
+team_b = (Wolves, Sheffield, Burnley, Liverpool)
 
 def get_match_winner():
-    team_one = fixtures.get('C2:C381')
-    team_two = fixtures.get('D2:D381')
-    for (a, b) in zip(team_one, team_two):
-        print(a, b)
-        print(f"Please enter the result of {a} against {b} \n")
-        print("Data should be two numbers seperated by a comma \n")
-        print("Example: 2,2 \n")
-        score_data = input("Enter the result here: \n")
-        
+
+    for (a, b) in zip(team_a, team_b):
+        team_one_score = input(f"Enter {a} score here: \n")
+        team_two_score = input(f"Enter {b} score here: \n")
+
+        if team_one_score > team_two_score:
+            cell_list = []
+            #get the headers from row #1
+            headers = table.row_values(1)
+            # find the column "Weight", we will remember this column #
+            colToUpdate = headers.index('Points')
+
+            # task 1 of 2
+            cellLookup = table.find(a)
+            # get the cell to be updated
+            cellToUpdate = table.cell(cellLookup.row, colToUpdate+1)
+            # update the cell's value
+            cellToUpdate.value = 3
+            # put it in the queue
+            cell_list.append(cellToUpdate)
+
+            # task 2 of 2
+            cellLookup = table.find(b)
+            # get the cell to be updated
+            cellToUpdate = table.cell(cellLookup.row, colToUpdate+1)
+            # update the cell's value
+            cellToUpdate.value = 0
+            # put it in the queue
+            cell_list.append(cellToUpdate)
+
+            # now, do it
+            table.update_cells(cell_list)
+
+        elif team_one_score == team_two_score:
+            cell_list = []
+            #get the headers from row #1
+            headers = table.row_values(1)
+            # find the column "Weight", we will remember this column #
+            colToUpdate = headers.index('Points')
+
+            # task 1 of 2
+            cellLookup = table.find(a)
+            # get the cell to be updated
+            cellToUpdate = table.cell(cellLookup.row, colToUpdate+1)
+            # update the cell's value
+            cellToUpdate.value = +1
+            # put it in the queue
+            cell_list.append(cellToUpdate)
+
+            # task 2 of 2
+            cellLookup = table.find(b)
+            # get the cell to be updated
+            cellToUpdate = table.cell(cellLookup.row, colToUpdate+1)
+            # update the cell's value
+            cellToUpdate.value = +1
+            # put it in the queue
+            cell_list.append(cellToUpdate)
+
+            # now, do it
+            table.update_cells(cell_list)
+            
+        else:
+            cell_list = []
+            #get the headers from row #1
+            headers = table.row_values(1)
+            # find the column "Weight", we will remember this column #
+            colToUpdate = headers.index('Points')
+
+            # task 1 of 2
+            cellLookup = table.find(a)
+            # get the cell to be updated
+            cellToUpdate = table.cell(cellLookup.row, colToUpdate+1)
+            # update the cell's value
+            cellToUpdate.value = +0
+            # put it in the queue
+            cell_list.append(cellToUpdate)
+
+            # task 2 of 2
+            cellLookup = table.find(b)
+            # get the cell to be updated
+            cellToUpdate = table.cell(cellLookup.row, colToUpdate+1)
+            # update the cell's value
+            cellToUpdate.value = +3
+            # put it in the queue
+            cell_list.append(cellToUpdate)
+
+            # now, do it
+            table.update_cells(cell_list)
+
 get_match_winner()
